bcfd9ae84387eef98ef78d22674dc5d09cd1bbc2
Mike Walsh <mpwalsh8@gmail.com>
2014-08-17 06:51:22 -0400

    More fixes for image paths and check for active sports.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,27 +15,27 @@ puts 'CREATED ADMIN USER: ' << user.email
 Sport.delete_all
 
 sports = [
-  { name: 'Baseball', imageurl: '/assets/pictograms/100x100/Baseball-Blk.png', active: false },
-  { name: 'Basketball', imageurl: '/assets/pictograms/100x100/Basketball-Blk.png', active: false },
-  { name: 'Cheerleading', imageurl: '/assets/pictograms/100x100/Cheerleading-Blk.png', active: false },
-  { name: 'Cross Country', imageurl: '/assets/pictograms/100x100/CrossCountry-Blk.png', active: false },
-  { name: 'Diving', imageurl: '/assets/pictograms/100x100/Diving-Blk.png', active: false },
-  { name: 'Field Hockey', imageurl: '/assets/pictograms/100x100/FieldHockey-Blk.png', active: false },
-  { name: 'Football', imageurl: '/assets/pictograms/100x100/Football-Blk.png', active: false },
-  { name: 'Golf', imageurl: '/assets/pictograms/100x100/Golf-Blk.png', active: false },
-  { name: 'Gymnastics', imageurl: '/assets/pictograms/100x100/Gymnastics-Blk.png', active: false },
-  { name: 'Ice Hockey', imageurl: '/assets/pictograms/100x100/IceHockey-Blk.png', active: false },
-  { name: 'Indoor Track', imageurl: '/assets/pictograms/100x100/IndoorTrack-Blk.png', active: false },
-  { name: 'Lacrosse', imageurl: '/assets/pictograms/100x100/Lacrosse-Blk.png', active: false },
-  { name: 'Softball', imageurl: '/assets/pictograms/100x100/Softball-Blk.png', active: false },
-  { name: 'Soccer', imageurl: '/assets/pictograms/100x100/Soccer-Blk.png', active: false },
-  { name: 'Sports Medicine', imageurl: '/assets/pictograms/100x100/FirstAid-Blk.png', active: false },
-  { name: 'Stunt', imageurl: '/assets/pictograms/100x100/Gymnastics-Blk.png', active: false },
-  { name: 'Swiming', imageurl: '/assets/pictograms/100x100/Swimming-Blk.png', active: false },
-  { name: 'Tennis', imageurl: '/assets/pictograms/100x100/Tennis-Blk.png', active: false },
-  { name: 'Track and Field', imageurl: '/assets/pictograms/100x100/TrackFieldPoleVault-Blk.png', active: false },
-  { name: 'Volleyball', imageurl: '/assets/pictograms/100x100/Volleyball-Blk.png', active: false },
-  { name: 'Wrestling', imageurl: '/assets/pictograms/100x100/Wrestling-Blk.png', active: false }
+  { name: 'Baseball', imageurl: 'pictograms/100x100/Baseball-Blk.png', active: false },
+  { name: 'Basketball', imageurl: 'pictograms/100x100/Basketball-Blk.png', active: false },
+  { name: 'Cheerleading', imageurl: 'pictograms/100x100/Cheerleading-Blk.png', active: false },
+  { name: 'Cross Country', imageurl: 'pictograms/100x100/CrossCountry-Blk.png', active: false },
+  { name: 'Diving', imageurl: 'pictograms/100x100/Diving-Blk.png', active: false },
+  { name: 'Field Hockey', imageurl: 'pictograms/100x100/FieldHockey-Blk.png', active: false },
+  { name: 'Football', imageurl: 'pictograms/100x100/Football-Blk.png', active: false },
+  { name: 'Golf', imageurl: 'pictograms/100x100/Golf-Blk.png', active: false },
+  { name: 'Gymnastics', imageurl: 'pictograms/100x100/Gymnastics-Blk.png', active: false },
+  { name: 'Ice Hockey', imageurl: 'pictograms/100x100/IceHockey-Blk.png', active: false },
+  { name: 'Indoor Track', imageurl: 'pictograms/100x100/IndoorTrack-Blk.png', active: false },
+  { name: 'Lacrosse', imageurl: 'pictograms/100x100/Lacrosse-Blk.png', active: false },
+  { name: 'Softball', imageurl: 'pictograms/100x100/Softball-Blk.png', active: false },
+  { name: 'Soccer', imageurl: 'pictograms/100x100/Soccer-Blk.png', active: false },
+  { name: 'Sports Medicine', imageurl: 'pictograms/100x100/FirstAid-Blk.png', active: false },
+  { name: 'Stunt', imageurl: 'pictograms/100x100/Gymnastics-Blk.png', active: false },
+  { name: 'Swiming', imageurl: 'pictograms/100x100/Swimming-Blk.png', active: false },
+  { name: 'Tennis', imageurl: 'pictograms/100x100/Tennis-Blk.png', active: false },
+  { name: 'Track and Field', imageurl: 'pictograms/100x100/TrackFieldPoleVault-Blk.png', active: false },
+  { name: 'Volleyball', imageurl: 'pictograms/100x100/Volleyball-Blk.png', active: false },
+  { name: 'Wrestling', imageurl: 'pictograms/100x100/Wrestling-Blk.png', active: false }
 ]
 
 for sport in sports
