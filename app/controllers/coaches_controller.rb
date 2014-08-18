bea0fd14407576748fe2d77c8f9d5d25e4cce323
Mike Walsh <mpwalsh8@gmail.com>
2014-08-17 22:50:00 -0400

    Fixed sort order.

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -4,7 +4,7 @@ class CoachesController < ApplicationController
   # GET /coaches
   # GET /coaches.json
   def index
-    @coaches = Coach.all
+    @coaches = Coach.all.order(:lastname, :firstname)
   end
 
   # GET /coaches/1
