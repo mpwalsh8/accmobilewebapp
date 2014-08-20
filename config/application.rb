9fad73753043bc36b9c2c0e6efc60de4301b10e4
Mike Walsh <mpwalsh8@gmail.com>
2014-08-20 16:02:03 -0400

    New banners added.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ACCMobileWebApp
     BANNERADS = [
       {
         :name => 'Apex Cougar Club',
-        :url => 'http://www.apexcougarclub.org/corporate-sponsorship/',
+        :url => 'http://www.apexcougarclub.org/corporate-sponsorship',
         :img => 'webapp/banner/Apex_Cougar_Club_320x50.svg'
       },
       {
@@ -33,58 +33,148 @@ module ACCMobileWebApp
       },
       {
         :name => 'Sports Clips Haircuts',
-        :url => 'http://haircutmenmorrisvillenc.com/',
+        :url => 'http://haircutmenmorrisvillenc.com',
         :img => 'webapp/banner/Sport_Clips_320x50.svg'
       },
       {
         :name => 'Spielberg Orthodontics',
-        :url => 'http://www.spielberg-ortho.com/',
+        :url => 'http://www.spielberg-ortho.com',
         :img => 'webapp/banner/Spielberg_Ortho_320x50.png'
       },
       {
         :name => 'Apex Lions Club',
-        :url => 'http://www.apexlions.org/',
+        :url => 'http://www.apexlions.org',
         :img => 'webapp/banner/Apex_Lions_Club_320x50.svg'
       },
       {
         :name => 'Apex Rotary Club',
-        :url => 'http://www.apexrotary.org/',
+        :url => 'http://www.apexrotary.org',
         :img => 'webapp/banner/Apex_Rotary_Club_320x50.svg'
       },
       {
         :name => 'Beavers Denistry',
-        :url => 'http://www.beaversdenistry.com/',
+        :url => 'http://www.beaversdenistry.com',
         :img => 'webapp/banner/Beavers_Denistry_320x50.svg'
       },
       {
         :name => 'Bo Jangles',
-        :url => 'http://www.bojangles.com/',
+        :url => 'http://www.bojangles.com',
         :img => 'webapp/banner/Bo_Jangles_320x50.svg'
       },
       {
         :name => 'Bradley Flooring',
-        :url => 'http://www.bradleyscarpet.com/',
+        :url => 'http://www.bradleyscarpet.com',
         :img => 'webapp/banner/Bradley_Flooring_320x50.svg'
       },
       {
         :name => 'Cat Kearns REMAX United',
-        :url => 'http://www.catkearns.remaxagent.com/',
+        :url => 'http://www.catkearns.remaxagent.com',
         :img => 'webapp/banner/Cat_Kearns_REMAX_320x50.svg'
       },
       {
         :name => 'Chick-Fil-a of Apex',
-        :url => 'http://www.chickfila.com/',
+        :url => 'http://www.chickfila.com',
         :img => 'webapp/banner/Chick-Fil-A_of_Apex_320x50.svg'
       },
       {
         :name => 'Cici\'s Pizza of Apex',
-        :url => 'http://www.cicispizza.com/',
+        :url => 'http://www.cicispizza.com',
         :img => 'webapp/banner/Cici_Pizza_of_Apex_320x50.svg'
       },
       {
-        :name => 'fresh Coat Painters',
-        :url => 'http://www.spielberg-ortho.com/',
-        :img => 'webapp/banner/Spielberg_Ortho_320x50.svg'
+        :name => 'Fresh Coat Painters',
+        :url => 'http://www.freshcoatpainters.net',
+        :img => 'webapp/banner/Fresh_Coat_Painters_320x50.svg'
+      },
+      {
+        :name => 'Apex Lions Club',
+        :url => 'http://www.apexlions.org',
+        :img => 'webapp/banner/Apex_Lions_Club_320x50.svg'
+      },
+      {
+        :name => 'Apex Rotary Club',
+        :url => 'http://www.apexrotary.org',
+        :img => 'webapp/banner/Apex_Rotary_Club_320x50.svg'
+      },
+      {
+        :name => 'Hendrick Cary Auto Mall',
+        :url => 'http://www.caryautomall.com',
+        :img => 'webapp/banner/Hendrick_Cary_Auto_Mall_320x50.svg'
+      },
+      {
+        :name => 'Hendrick Toyota Scion of Apex',
+        :url => 'http://www.hendricktoyotaapex.com',
+        :img => 'webapp/banner/Hendrick_Toyota_Scion_of_Apex_320x50.svg'
+      },
+      {
+        :name => 'JD\'s Tavern',
+        :url => 'http://www.jdstavern.com',
+        :img => 'webapp/banner/JDs_Tavern_320x50.svg'
+      },
+      {
+        :name => 'Kangaroo Express',
+        :url => 'http://kangarooexpress.com/StoreHome/846',
+        :img => 'webapp/banner/Kangaroo_Express_320x50.svg'
+      },
+      {
+        :name => 'L.G. Jordan Oil Co.',
+        :url => 'http://www.lgjordanoil.com',
+        :img => 'webapp/banner/LGJordan_Oil_Co_320x50.svg'
+      },
+      {
+        :name => 'Luihn Food Systems KFC',
+        :url => 'http://www.kfc.com',
+        :img => 'webapp/banner/Luihn_Food_Systems_KFC_320x50.svg'
+      },
+      {
+        :name => 'Luihn Food Systems Taco Bell',
+        :url => 'http://www.tacobell.com',
+        :img => 'webapp/banner/Luihn_Food_Systems_Taco_Bell_320x50.svg'
+      },
+      {
+        :name => 'McDonalds of Apex',
+        :url => 'http://www.mcnorthcarolina.com/17721',
+        :img => 'webapp/banner/McDonalds_of_Apex_320x50.svg'
+      },
+      {
+        :name => 'Metcalf Painting & Interiors',
+        :url => 'http://www.metcalfpainting.com',
+        :img => 'webapp/banner/Metcalf_Painting_320x50.svg'
+      },
+      {
+        :name => 'Mosquito Authority',
+        :url => 'http://www.bugsbite.com',
+        :img => 'webapp/banner/Mosquito_Authority_320x50.svg'
+      },
+      {
+        :name => 'Perfect Promotions',
+        :url => 'http://www.perfect-promos.com',
+        :img => 'webapp/banner/Perfect_Promotions_320x50.svg'
+      },
+      {
+        :name => 'Red Robin',
+        :url => 'http://www.redrobin.com',
+        :img => 'webapp/banner/.svg'
+      },
+      {
+        :name => 'Scruffy Duck',
+        :url => 'http://www.scruffy-duck.com',
+        :img => 'webapp/banner/Scruffy_Duck_320x50.svg'
+      },
+      {
+        :name => 'The Skin Institute',
+        :url => 'http://www.tsiskin.com',
+        :img => 'webapp/banner/The_Skin_Institute_320x50.svg'
+      },
+      {
+        :name => 'Triangle Eye Institute',
+        :url => 'http://www.tei2020.com',
+        :img => 'webapp/banner/'
+      },
+      {
+        :name => 'Triangle Orthopaedics',
+        :url => 'http://www.triangleortho.com',
+        :img => 'webapp/banner/Triangle_Orthopaedics_320x50.svg'
       }
     ]
     SPLASHADS = [
