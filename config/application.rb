b3a2aac26165cf7bdcbc3d74e3738c96629ed18e
Mike Walsh <mpwalsh8@gmail.com>
2014-08-19 23:26:11 -0400

    More banner work.

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module ACCMobileWebApp
       },
       {
         :name => 'Kona-Ice',
-        :url => 'http://www.kona-ice.com',
+        :url => 'http://www.kona-ice.com/find-a-kona/local-site/353',
         :img => 'webapp/banner/Kona_Ice_320x50.svg'
       },
       {
@@ -40,6 +40,51 @@ module ACCMobileWebApp
         :name => 'Spielberg Orthodontics',
         :url => 'http://www.spielberg-ortho.com/',
         :img => 'webapp/banner/Spielberg_Ortho_320x50.png'
+      },
+      {
+        :name => 'Apex Lions Club',
+        :url => 'http://www.apexlions.org/',
+        :img => 'webapp/banner/Apex_Lions_Club_320x50.svg'
+      },
+      {
+        :name => 'Apex Rotary Club',
+        :url => 'http://www.apexrotary.org/',
+        :img => 'webapp/banner/Apex_Rotary_Club_320x50.svg'
+      },
+      {
+        :name => 'Beavers Denistry',
+        :url => 'http://www.beaversdenistry.com/',
+        :img => 'webapp/banner/Beavers_Denistry_320x50.svg'
+      },
+      {
+        :name => 'Bo Jangles',
+        :url => 'http://www.bojangles.com/',
+        :img => 'webapp/banner/Bo_Jangles_320x50.svg'
+      },
+      {
+        :name => 'Bradley Flooring',
+        :url => 'http://www.bradleyscarpet.com/',
+        :img => 'webapp/banner/Bradley_Flooring_320x50.svg'
+      },
+      {
+        :name => 'Cat Kearns REMAX United',
+        :url => 'http://www.catkearns.remaxagent.com/',
+        :img => 'webapp/banner/Cat_Kearns_REMAX_320x50.svg'
+      },
+      {
+        :name => 'Chick-Fil-a of Apex',
+        :url => 'http://www.chickfila.com/',
+        :img => 'webapp/banner/Chick-Fil-A_of_Apex_320x50.svg'
+      },
+      {
+        :name => 'Cici\'s Pizza of Apex',
+        :url => 'http://www.cicispizza.com/',
+        :img => 'webapp/banner/Cici_Pizza_of_Apex_320x50.svg'
+      },
+      {
+        :name => 'fresh Coat Painters',
+        :url => 'http://www.spielberg-ortho.com/',
+        :img => 'webapp/banner/Spielberg_Ortho_320x50.svg'
       }
     ]
     SPLASHADS = [
