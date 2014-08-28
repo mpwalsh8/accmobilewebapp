868d93290ed4dbb01ec5b2507727a62ccf6cc8cb
Mike Walsh <mpwalsh8@gmail.com>
2014-08-27 21:01:11 -0400

    Events workoing.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,19 @@
 class Event < ActiveRecord::Base
   has_and_belongs_to_many :teams, through: :events
   has_and_belongs_to_many :opponents, through: :events
+
+  def datetime
+    dt = DateTime.parse(sprintf("%s %s", eventdate, eventtime))
+    dt.strftime("%b %-d %Y @ %l:%M %p")
+  end
+
+  def datetimeshort
+    dt = DateTime.parse(sprintf("%s %s", eventdate, eventtime))
+    dt.strftime("%m/%-d/%Y")
+  end
+
+  def datetimelong
+    dt = DateTime.parse(sprintf("%s %s", eventdate, eventtime))
+    dt.strftime("%A, %B %-d %Y at %l:%M %p")
+  end
 end
