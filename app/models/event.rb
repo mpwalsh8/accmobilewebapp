6f8584f18ba67b2e52b3d5c209337ca22d99e0ca
Mike Walsh <mpwalsh8@gmail.com>
2014-09-22 13:35:53 -0400

    Added better validation.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
   serialize :opponents
-  validates :venueid, :presence => true
+  validates :status, :presence => { :message => 'Status must be specified.' }
+  validates :venueid, :presence => { :message => 'Venue must be specified.' }
+  validates :eventlocation, :presence => { :message => 'Location must be specified.' }
   validates :eventdate, :presence => { :message => 'Event Date is required.' }
   validates :eventtime, :presence => { :message => 'Event Time is required.' }
   validates_format_of :eventdate, :message => 'Date must be YYYY-MM-DD format.', :with => /(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])/
