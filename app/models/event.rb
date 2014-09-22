c6b32436af90af87e51ba528c0e4477145d03083
Mike Walsh <mpwalsh8@gmail.com>
2014-09-22 11:47:05 -0400

    Changes to tweak forms to better handle error messages.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,11 @@
 class Event < ActiveRecord::Base
   serialize :opponents
+  validates :venueid, :presence => true
+  validates :eventdate, :presence => { :message => 'Event Date is required.' }
+  validates :eventtime, :presence => { :message => 'Event Time is required.' }
+  validates_format_of :eventdate, :message => 'Date must be YYYY-MM-DD format.', :with => /(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])/
+  validates_format_of :eventtime, :message => 'Time must be HH:MM format.', :with => /([01]?[0-9]|2[0-3]):[0-5][0-9]/
+
 
   #has_and_belongs_to_many :teams, through: :events
   #has_and_belongs_to_many :opponents, through: :events
