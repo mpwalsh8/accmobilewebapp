0a09a3b974bf811733d8266163903a19d730a5b6
Mike Walsh <mpwalsh8@gmail.com>
2015-08-29 16:32:03 -0400

    Added error checking for Ad creation.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,2 +1,6 @@
 class Ad < ActiveRecord::Base
+  validates :name, :presence => { :message => 'Name must be specified.' }
+  validates :imageurl, :presence => { :message => 'Image URL must be specified.' }
+  validates :clickurl, :presence => { :message => 'Click URL must be specified.' }
+  validates :adtype, :presence => { :message => 'Ad Type must be specified.' }
 end
